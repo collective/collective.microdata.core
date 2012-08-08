dbf331183e7e1e081a85e72362840b177d1f9cc8
Luca Fabbri <luca@keul.it>
2012-08-08 13:14:07 +0200

    first commit

---
+++ collective/microdata/core/interfaces.py
@@ -0,0 +1,5 @@
+from zope.interface import Interface
+
+class IMicrodataCoreLayer(Interface):
+    """Marker interface for the collective.microdata.event layer"""
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.microdata.core',
+      version=version,
+      description="Core package for Plone microdata support",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone microdata schema.org html5 event',
+      author='keul',
+      author_email='luca@keul.it',
+      url='https://github.com/keul/collective.microdata.core',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.microdata'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
