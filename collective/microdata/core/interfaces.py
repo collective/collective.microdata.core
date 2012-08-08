691400993803d0ac7fe378d52a649d0e2d79e072
Luca Fabbri <luca@keul.it>
2012-08-08 15:52:02 +0200

    added Event schema.org interface

--- collective/microdata/core/interfaces.py
+++ collective/microdata/core/interfaces.py
@@ -1,5 +1,32 @@
 from zope.interface import Interface
+from zope import schema
 
 class IMicrodataCoreLayer(Interface):
     """Marker interface for the collective.microdata.event layer"""
 
+
+class ISchemaOrgThing(Interface):
+    """See http://schema.org/Thing"""
+    
+    additionalType = schema.URI(title=u"Additional type",
+                                description=u"An additional type for the item, "
+                                            u"typically used for adding more "
+                                            u"specific types from external "
+                                            u"vocabularies in microdata syntax.",
+                                required=False)
+
+    description = schema.Text(title=u"Description",
+                              description=u"A short description of the item.",
+                              required=False)
+
+    image = schema.URI(title=u"Image",
+                       description=u"URL of an image of the item.",
+                       required=False)
+
+    name = schema.TextLine(title=u"Name",
+                           description=u"The name of the item.",
+                           required=True)
+
+    url = schema.URI(title=u"URL",
+                     description=u"URL of the item.",
+                     required=True)
